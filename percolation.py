65a6a7a76ccaac24154c16b31ed8c2de9ffbd79e
Camille Perlada <perladacamille@gmail.com>
2016-12-03 21:51:57 +0800

    Final Code

--- percolation.py
+++ percolation.py
@@ -16,62 +16,56 @@ class Percolation(object):
         
     #Count the number and size of percolating clusters
     def mass(self,label): 
-        elements = np.unique(label, return_counts = 1)
-        counts = np.unique(label, return_counts = 1)        
+        elements, counts = np.unique(label, return_counts = 1)      
         return np.mean(counts[elements!=0])
         
     #Hoshen-Kopelman algorithm
-    def labelling(self,N,p):
-        data = self.lattice(N,p)
+    def labelling(self, n, p): #Create the clusters and label them
+        data = self.lattice(n,p)
         label = np.zeros_like(data, dtype = int)
-        
-        #Set the label of clustering sites that thouches the edge to -1
+
+        #Set the label of clustering sites that thouches the edge to -1    
         label[:,0] = data[:,0]*-1
         label[0,:] = data[0,:]*-1
-        label[:,N-1] = data[:,N-1]*-1
-        label[N-1,:] = data[N-1,:]*-1
-        
-        for i in xrange(1,N-1):
-            for j in xrange(1,N-1):
+        label[:,n-1] = data[:,n-1]*-1
+        label[n-1,:] = data[n-1,:]*-1
+        for i in xrange(1,n-1):
+            for j in xrange(1,n-1):            
             # occupied
                 if data[i,j]: 
-                    #compare with up and left
-                    if data[i-1, j] and not(data[i,j-1]): # up occupied
+                    if data[i-1, j] and not(data[i,j-1]): #up occupied
                         label[i,j] = label[i-1,j]
-                    
                     elif data[i, j-1] and not(data[i-1, j]): # left occupied
                         label[i,j] = label[i, j-1]
-                    
+                        
                     elif not(data[i,j-1]) and not(data[i-1, j]): # none occupied
                         label[i,j] = np.max(label) + 1
-                    
+                        
                     elif data[i-1, j] and data[i, j-1]: # both occupied
                         if label[i-1,j] < label[i, j-1]:
                             label[i,j] = label[i-1,j] # choose smaller label
-                            label[label == label[i, j-1]] = label[i-1,j]
-                        
+                            label[label == label[i, j-1]] = label[i-1,j] 
+                            
                         elif label[i-1,j] > label[i, j-1]:
                             label[i,j] = label[i,j-1] # choose smaller label
                             label[label == label[i-1, j]] = label[i,j-1]
-                        
+                            
                         else:
                             label[i,j] = label[i, j-1] 
-                    
-                    if i == N-2:
-                        if label[i+1, j] == -1: # if touching the edge
+                            
+                    if i == n-2: # if touching the edge
+                        if label[i+1, j] == -1:
                             label[label == label[i,j]] = -1
-                    
-                    if j == N-2:
-                        if label[i, j+1] == -1: # if touching the edge
+                            
+                    if j == n-2: # if touching the edge
+                        if label[i, j+1] == -1:
                             label[label == label[i,j]] = -1
-        
-        label[label==-1] = 0 # touching the edge
-        
+        label[label==-1] = 0
         clustersize = self.mass(label)
         return data, label, clustersize
     
     #percolation threshold
-    def thresh(self,N = 50,trials = 10):
+    def thresh(self,n = 50,trials = 10):
         pl = np.arange(0, 1.1, 0.01)
         massl = np.empty_like(pl)
         for w in xrange(0, 100):
@@ -80,7 +74,7 @@ class Percolation(object):
             for _ in xrange(trials):
                 print 'trial', len(massp)
                 print 'p= ', p
-                __, __, clustersize = self.labelling(N,p)
+                __, __, clustersize = self.labelling(n,p)
                 massp.append(clustersize)
             if len(massp) != 0:
                 massl[w] = np.mean(massp)
@@ -93,22 +87,22 @@ class Percolation(object):
         plt.figure()
         plt.imshow(data, cmap = 'gray')
         plt.title('Percolation at p = %s' %(0.59275))
-#        plt.savefig('PercolationPlot.pdf', dpi = 300, bbox_inches = 'tight')
+
         plt.figure()
         plt.imshow(label)
         plt.title('Size clusters at p = %s' %(0.59275))
-#        plt.savefig('LabellingPlot.pdf', dpi = 300, bbox_inches = 'tight')
+
 
     def Percothreshplot(self):
-        pl,massl = self.thresh(N = 500, trials = 10)
+        pl,massl = self.thresh(n = 500, trials = 10)
         pthreshold = pl[massl == np.max(massl)]
-        print 'p_c= ', pthreshold #determine critical concentration
+        print 'p_c= ', pthreshold 
         plt.figure()        
-        plt.plot(pl, massl,'--ro', lw = 3, ms = 6, label = '$p_c=$ %s' %(pthreshold))
-        plt.legend(loc = 0, prop = {'size':15},framealpha = 0.5, fancybox = True)
-        plt.xlabel('Concentration, $\mathbf{p}$', fontsize = 18, family = 'serif', va = 'top')
-        plt.ylabel('Cluster Mass, $\mathbf{M}$', fontsize = 18, family = 'serif', va = 'bottom', rotation = 'vertical')               
-#        plt.savefig('Mvsp.pdf', dpi = 300, bbox_inches = 'tight')    
+        plt.plot(pl, massl,'--ro', lw = 2, ms = 6)
+        plt.xlabel('$\mathbf{p}$', fontsize = 12) #Concentration
+        plt.ylabel('$\mathbf{M}$', fontsize = 12) #Cluster Mass            
+ 
+        
 
 if __name__ == "__main__":
     Sim = Percolation()
